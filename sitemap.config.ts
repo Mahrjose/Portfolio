9ba1208c8e00b17000cdf4b6cfa057e2aa20f602
Mirza Mahrab Hossain <mahrjose.dev@gmail.com>
2025-06-14 03:49:02 +0600

    feat: add SEO and sitemap configuration for improved site visibility

---
+++ sitemap.config.ts
@@ -0,0 +1,13 @@
+module.exports = {
+  siteUrl: "https://mahrabhossain.me",
+  generateRobotsTxt: true,
+  sitemapSize: 50,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+  },
+};
